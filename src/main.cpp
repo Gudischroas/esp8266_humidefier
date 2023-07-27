ade3e6e97d1fc0503df326cf94dcb44886cf34d6
zbyisgudi <zhang_bingyang@outlook.com>
2023-07-27 14:57:35 +0800

    project created

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include <Arduino.h>
+#include "DHT.h"  //dht11 sensor library
+#include <Wire.h> //i2c library
+
+// put function declarations here:
+
+char WIFI_information(char);
+
+void setup() {
+  // put your setup code here, to run once:
+  int result = WIFI_information();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
+
+// put function definitions here:
+char WIFI_information(char flag) {
+  return ;
+}
